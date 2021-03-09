e0b7361c7e055079285ef1fd6acf1393aadb1559
namahxverma <78422591+namahxverma@users.noreply.github.com>
2021-03-09 19:13:48 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,17 @@
+class dustbin {
+  constructor(x, y, width, height) {
+    this.body = Bodies.rectangle(x, y, width, height,{isStatic:true});
+    this.width = width ; 
+    this.height = height;
+
+    World.add(world, this.body);
+  }
+
+  display() {
+    var pos = this.body.position;
+
+    rectMode(CENTER);
+    fill("green");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+}
